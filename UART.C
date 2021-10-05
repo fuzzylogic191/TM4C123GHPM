247cb2dfa0fe66e74c0bc477c56e46bb60e11e0e
Gregory Kuhn <67237034+fuzzylogic191@users.noreply.github.com>
2021-10-05 05:21:29 -0600

    Update UART.C

--- UART.C
+++ UART.C
@@ -1,1 +1,13 @@
-
+int main(void)
+{
+  char c;
+  //Enable the UART module
+  SYSCTL->RCGCUART |= (1<<0);
+  //Enable the clock
+  SYSCTL->RCGCPIO |= (1<<0);
+  //
+  GPIOA0->AFSEL = (1<<1) || (1<<0);
+  
+  GPIOA0->PCTDDDDDDD|| (1<<0);
+ 
+}
